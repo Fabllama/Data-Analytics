ccee26b90596c9168aec5044f9549cc5a947c9cd
Fabllama <fabllama1@gmail.com>
2025-02-12 15:59:01 +1300

    activity 1

--- Week 10/01-Exercise Distribution Plots.py
+++ Week 10/01-Exercise Distribution Plots.py
@@ -6,12 +6,9 @@ sample1 = pd.read_csv('Sample_Data_for_Activity.csv')
 
 print("Print top 5 data : \n",sample1.head)
 
-# sns.displot(sample1['Uniform_Distribution'], kde=True, color='purple', bins=50)
-# sns.displot(sample1['Normal_Distribution'], kde=True, color='red', bins=20)
-# sns.displot(sample1['Exponential_Distribution'], kde=True, color='grey', bins=20)
-# sns.displot(sample1['Poisson_Distribution'], kde=True, color='green', bins=20)
+sns.displot(sample1['Uniform_Distribution'], kde=True, color='purple', bins=50)
+sns.displot(sample1['Normal_Distribution'], kde=True, color='red', bins=20)
+sns.displot(sample1['Exponential_Distribution'], kde=True, color='grey', bins=20)
+sns.displot(sample1['Poisson_Distribution'], kde=True, color='green', bins=20)
 
-# plt.show()
-
-# import os
-# print('here :'os.getcwd())
+plt.show()
